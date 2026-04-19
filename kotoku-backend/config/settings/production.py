b19e9d468592f1423b75e0819a1b7f6ce6e482e0
Samuel Danquah Ankapong <hellosamdanq@gmail.com>
2026-04-19 10:22:36 +0000

    feat: add logging, health/readiness endpoints, and object storage config

--- kotoku-backend/config/settings/production.py
+++ kotoku-backend/config/settings/production.py
@@ -5,3 +5,14 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"  # noqa: F405
+AWS_QUERYSTRING_EXPIRY = 3600
