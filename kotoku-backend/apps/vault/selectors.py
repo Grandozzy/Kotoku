8fc8ad159967dee21ffc28a84edfc2748b037f52
Samuel Danquah Ankapong <hellosamdanq@gmail.com>
2026-05-07 07:33:05 +0000

    fix: vault detail MultipleObjectsReturned from party join

--- kotoku-backend/apps/vault/selectors.py
+++ kotoku-backend/apps/vault/selectors.py
@@ -10,7 +10,7 @@ class VaultSelector:
     ) -> VaultEntry:
         qs = VaultEntry.objects.select_related(
             "agreement", "agreement__created_by"
-        )
+        ).filter(agreement_id=agreement_id)
         if account_phone:
             qs = qs.filter(
                 Q(agreement__created_by__pk=account_id)
@@ -18,7 +18,7 @@ class VaultSelector:
             )
         else:
             qs = qs.filter(agreement__created_by__pk=account_id)
-        return qs.get(agreement_id=agreement_id)
+        return qs.distinct().get()
 
     @staticmethod
     def list_for_account(*, account_id: int, account_phone: str = None) -> QuerySet:
